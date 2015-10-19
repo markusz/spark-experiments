351ee1f83e871b4af44e94766df78f50b5d5048c
Markus Ziller <markus.ziller@netlight.com>
2015-10-20 00:33:00 +0200

    Adds mutlithreading with 3 cores

--- src/main/scala/SQL.scala
+++ src/main/scala/SQL.scala
@@ -13,22 +13,22 @@ object SQL {
     val username = "root"
     val password = ""
 
-    val config = new SparkConf().setMaster("local").setAppName("Test App")
+    val config = new SparkConf().setMaster("local[3]").setAppName("Test App")
     val sc = new SparkContext(config)
     val sqlContext = new SQLContext(sc)
     val hiveContext = new HiveContext(sc)
 
     val mySQLUrl: String = url + "/?user=" + username + "&password=" + password
-    var schema = "adextern"
-    var tablename = "psd_adextern_brands"
+    var schema = "ivw_prd"
+    var tablename = "mineus_ivw_path"
     val jdbcDF = hiveContext.read.format("jdbc").options(
       Map("url" -> mySQLUrl,
         "dbtable" -> (schema + "." + tablename)
       )).load()
 
-    jdbcDF.cache()
-    val first_names = jdbcDF.select("name")
-    first_names.show(false)
-    println(jdbcDF.count())
+    jdbcDF.groupBy("name").count().show()
+//    jdbcDF.cache()
+//    val names = jdbcDF.select("name")
+//    println(jdbcDF.count())
   }
 }
